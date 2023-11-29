dcc1bfcd331c4b5fd2c6524669ebd5ef444a1e6f
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-11-29 23:47:43 +0900

    style: hide button of header on search page

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,1 +1,10 @@
+import "../scss/main.scss";
+
 const header = document.querySelector("header");
+const currentPath = window.location.pathname;
+
+if (currentPath === "/search" && header) {
+  header.children[1].className = "hidden";
+} else if (header) {
+  header.children[1].className = "";
+}
