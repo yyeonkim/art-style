d7555d0eccee3a8e73ab42e7ad0ddba8d29beb4c
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-12-01 13:35:03 +0900

    style: resize image

--- src/client/ts/header.ts
+++ src/client/ts/header.ts
@@ -1,8 +1,9 @@
 const header = document.querySelector("header");
 
 hiddenSearch();
+addShadow();
 
-/* 현재 브라우저 경로에 따라 이미지 검색 버튼 숨기기 */
+/* 브라우저 경로에 따라 이미지 검색 버튼 숨기기 */
 function hiddenSearch() {
   const currentPath = window.location.pathname;
 
@@ -10,5 +11,16 @@ function hiddenSearch() {
     header.children[1].className = "hidden";
   } else if (header) {
     header.children[1].className = "";
+  }
+}
+
+/* 브라우저 경로에 따라 그림자 스타일 추가 */
+function addShadow() {
+  const currentPath = window.location.pathname;
+
+  if (currentPath !== "/" && header) {
+    header.className = "shadow";
+  } else if (header) {
+    header.className = "";
   }
 }
