007548493d798640370fee940b878c1ec4a6370d
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-06 16:39:07 +0900

    chore: 타입 변경

--- src/client/ts/api.ts
+++ src/client/ts/api.ts
@@ -1,7 +1,14 @@
+import { LABEL } from "../../constants";
 import { IArtwork } from "../../types";
 
 /* 카테고리별 작품 이미지 API 요청 */
-async function getArtwork(category: string): Promise<IArtwork[]> {
+async function getArtwork(
+  category:
+    | LABEL.REALIST
+    | LABEL.POST_IMPRESSIONIST
+    | LABEL.IMPRESSIONIST
+    | LABEL.ROMANTIC
+): Promise<IArtwork[]> {
   const json = await fetch(`/api/artworks?category=${category}`).then((res) =>
     res.json()
   );
